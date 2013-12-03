b1188649a8c103eb9dbf88e0f3316d1d99244cdf
Carey Janecka <figitaki@Careys-Mac.local>
2013-12-03 13:19:23 -0800

    Working on interaction

--- scripts/chess.js
+++ scripts/chess.js
@@ -1,16 +1,16 @@
-var WHITE_KING = 100;
-var WHITE_QUEEN = 90;
-var WHITE_ROOK = 50;
-var WHITE_BISHOP = 31;
-var WHITE_KNIGHT = 30;
-var WHITE_PAWN = 10;
+var WHITE_KING = 0x01;
+var WHITE_QUEEN = 0x02;
+var WHITE_ROOK = 0x03;
+var WHITE_BISHOP = 0x05;
+var WHITE_KNIGHT = 0x06;
+var WHITE_PAWN = 0x07;
  
-var BLACK_KING = -WHITE_KING;
-var BLACK_QUEEN = -WHITE_QUEEN;
-var BLACK_ROOK = -WHITE_ROOK;
-var BLACK_BISHOP = -WHITE_BISHOP;
-var BLACK_KNIGHT = -WHITE_KNIGHT;
-var BLACK_PAWN = -WHITE_PAWN;
+var BLACK_KING = 0x09;
+var BLACK_QUEEN = 0x0A;
+var BLACK_ROOK = 0x0B;
+var BLACK_BISHOP = 0x0D;
+var BLACK_KNIGHT = 0x0E;
+var BLACK_PAWN = 0x0F;
 
 var board = [[BLACK_ROOK, BLACK_KNIGHT, BLACK_BISHOP, BLACK_QUEEN, BLACK_KING, BLACK_BISHOP, BLACK_KNIGHT, BLACK_ROOK],
              [BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN],
@@ -56,6 +56,21 @@ function getPieceName(peiceValue) {
 	}
 }
 
+var background_color;
+var selected_color;
+
 $(function() {
 	drawBoard(board);
+	$('.column').hover(function() {
+		background_color = $(this).css('background');
+		$(this).css('background', '#b93939');
+	}, function() {
+		if(!$(this).hasClass('selected'))
+			$(this).css('background', background_color);
+	});
+	$('.column').click(function() {
+		selected_color = $(this).css('background');
+		$(this).css('background', '#bbbb00');
+		$(this).addClass('selected')
+	})
 });
